6c885a3cc627015558eec7ec8deb2d1814b9b6fc
José Luis Martínez Cadavid <jlmc97@gmail.com>
2019-11-06 15:53:18 -0500

    Small changes

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+from mpi4py import MPI
+import math
+import time
+import datetime
+import copy
+import hashlib
+
+import primeBlockchain
+
--- primeBlockchain.py
+++ primeBlockchain.py
@@ -2,7 +2,8 @@ import numpy as np
 from mpi4py import MPI
 import math
 import time
-
+import datetime
+import copy
 import hashlib
 
 class MinimalBlock():
